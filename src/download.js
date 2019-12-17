8d640ef5e6ae4aad5efe119f33adfa32b3720939
niels <niels.vredeveldt@live.nl>
2019-12-17 20:54:28 +0100

    created handshake and checked id of message

--- src/download.js
+++ src/download.js
@@ -3,27 +3,39 @@
 const net = require('net');
 const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
+const message = require('./message');
 
 module.exports = torrent => {
     tracker.getPeers(torrent, peers => {
-        peers.forEach(download);
+        peers.forEach(peer => download(peer, torrent));
 });
 }
 
-function download(peer){
+function download(peer, torrent){
     const socket = net.Socket();
     socket.on('error', console.log);
     socket.connect(peer.port,peer.ip, () => {
-        // socket.write a message here
+        socket.write(message.buildHandshake(torrent));
     });
-    onWholeMsg(socket, data => {
-        //handle response here 
-    });
+    onWholeMsg(socket, msg => msgHandler(msg,socket));
+}
 
+function msgHandler(msg, socket){
+    if(isHandshake(msg)){
+        socket.write(message.buildInterested());
+    } 
+    else {
+        const m = message.parse(msg);
+        if (m.id === 0) chokeHandler();
+        if (m.id === 1) unchokeHandler();
+        if (m.id === 4) haveHandler(m.payload);
+        if (m.id === 5) bitfieldHandler(m.payload);
+        if (m.id === 7) pieceHandler(m.payload);
+    }
+}
 
-    socket.on('data', data => {
-        // handle response here
-    })
+function isHandshake(msg){
+    return msg.length === msg.readUInt8(0) + 49 && msg.toString('utf8',1) === 'BitTorrent protocol';
 }
 
 function onWholeMsg(socket, callback){
@@ -32,6 +44,14 @@ function onWholeMsg(socket, callback){
 
     socket.on('data', recvBuf => {
         //msgLen calculates length of a whole message
-        msgLen = () => 
-    })
+        msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 :
+        savedBuf.readInt32BE(0) + 4;
+        savedBuf = Buffer.concat([savedBuf,recvBuf]);
+
+        while (savedBuf.length >= 4 && savedBuf.length >= msgLen()){
+            callback(savedBuf.slice(0,msgLen()));
+            savedBuf = savedBuf.slice(msgLen());
+            handshake = false;
+        }
+    });
 }
